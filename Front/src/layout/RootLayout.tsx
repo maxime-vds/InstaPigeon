92c77f463d5db418f32f638d43ccc0fb9ad83cde
maxime-vds <maxime.vandriessche@hotmail.be>
2023-03-23 12:06:28 +0100

    noothing

--- Front/src/layout/RootLayout.tsx
+++ Front/src/layout/RootLayout.tsx
@@ -5,11 +5,11 @@ export default function RootLayout() {
         <div className="root-layout">
                 <header>
                     <nav>
-                        {/* <h1>Jobarouter</h1>
+                        <h1>Jobarouter</h1>
                         <NavLink to="/">Home</NavLink>
                         <NavLink to="about">About</NavLink>
                         <NavLink to="help">Help</NavLink>
-                        <NavLink to="careers">Careers</NavLink> */}
+                        <NavLink to="careers">Careers</NavLink> 
                     </nav>
             </header>
             <main>
