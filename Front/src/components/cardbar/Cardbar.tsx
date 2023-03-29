4ca8c1302b0c7a4aaec962f7ee84be380ba60351
Ricky <jacobus.kim@gmail.com>
2023-03-29 15:02:44 +0200

    PATCH method works

--- Front/src/components/cardbar/Cardbar.tsx
+++ Front/src/components/cardbar/Cardbar.tsx
@@ -5,20 +5,36 @@ import { useState } from "react";
 
 
 const Cardbar = () => {
+
+  const [caption, setCaption] = useState<string>("what's up")
   const [likes, setLikes] = useState<Number>(999)
   const {id} = useParams()
-  const url = "http://localhost:3000/posts" + id  
-  const {data, isPending, error, postData} = useFetch(url, 'PUT')
+  const url = "http://localhost:3000/posts/" + id  
 
 
 function postLikes(e: { preventDefault: () => void; }) {
   e.preventDefault()
   console.log("click love")
 
-  postData({likes})
+  const putLikes = {
+    method : "PATCH",
+    headers : {
+      "Content-type": "application/json",
+    },
+    body : JSON.stringify({likes}),
+  }
+  fetch(url, putLikes)
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
+
+}
+
+
+
+
 
   // fetch and post to db
-}
 
 
 function postFollows() {
@@ -39,7 +55,7 @@ function postFollows() {
   
   
   )
-}
+  }
 
 export default Cardbar
 
