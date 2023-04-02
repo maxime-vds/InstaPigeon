d91d28d7ad529e6a12c5f5803306688e9af7709b
Ricky <jacobus.kim@gmail.com>
2023-04-02 15:03:18 +0200

    prettier config

--- Front/src/pages/Account/AccountSettings.tsx
+++ Front/src/pages/Account/AccountSettings.tsx
@@ -1,31 +1,25 @@
-import { Button } from "../../components/button/Button"
-import CircleIcon from "@mui/icons-material/Circle"
+import { Button } from '../../components/button/Button'
+import CircleIcon from '@mui/icons-material/Circle'
 
 interface IsettingsNavProp {
-  settingsNav?: boolean
-  setSettingsNav: React.Dispatch<React.SetStateAction<boolean>>
+   settingsNav?: boolean
+   setSettingsNav: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export const AccountSettings = ({ setSettingsNav }: IsettingsNavProp) => {
-  const handleClick = () => {
-    setSettingsNav(false)
-  }
+   const handleClick = () => {
+      setSettingsNav(false)
+   }
 
-  return (
-    <>
-      <h2>Account Settings</h2>
-      <div>
-          <CircleIcon sx={{ fontSize: "500%" }} />
-          <p>Ricky Matato</p>
-        </div>
+   return (
+      <>
+         <h2>Account Settings</h2>
+         <div>
+            <CircleIcon sx={{ fontSize: '500%' }} />
+            <p>Ricky Matato</p>
+         </div>
 
-
-
-
-
-
-
-      <Button onClick={handleClick} buttonText="Return"></Button>
-    </>
-  )
+         <Button onClick={handleClick} buttonText="Return"></Button>
+      </>
+   )
 }
