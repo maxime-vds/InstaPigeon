24de1d09829ae9ecb1dc42d961704cdd9a986b7d
Ricky <jacobus.kim@gmail.com>
2023-04-04 15:32:21 +0200

    add motion to modal

--- Front/src/components/BasicModal.tsx
+++ Front/src/components/BasicModal.tsx
@@ -1,40 +1,48 @@
-import * as React from "react"
-import Box from "@mui/material/Box"
-import Typography from "@mui/material/Typography"
-import Modal from "@mui/material/Modal"
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
+import { motion } from 'framer-motion'
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  outline: 0,
-  transform: "translate(-50%, -50%)",
-  width: "200px",
-  bgcolor: "#D9D9D9",
-  boxShadow: 24,
-  borderRadius: "3px",
-  p: 5,
+import styles from './BasicModal.module.css'
+
+const modalVariants = {
+   hidden: {
+    y:1000,
+    x: 0
+   },
+   visible: {
+    x:0,
+    y:0,
+   
+    transition: {
+      delay: 0.5
+    }
+   },
 }
 
 interface IbasicModalProps {
-  open: boolean
-  setOpen: (a: boolean) => void
+   open: boolean
+   setOpen: (a: boolean) => void
 }
 
 export default function BasicModal({ open, setOpen }: IbasicModalProps) {
-  const handleClose = () => setOpen(false)
+   const handleClose = () => setOpen(false)
 
-  return (
-    <div>
-      <Modal open={open} onClose={handleClose}>
-        <Box sx={style}>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Cookies are great and so are you!
-          </Typography>
+   return (
+      <div>
+         <Modal open={open} onClose={handleClose}>
+            <motion.div className={styles['basic-modal']}
+            variants={modalVariants}
+            initial="hidden"
+            animate="visible">
+               <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  Cookies are great and so are you!
+               </Typography>
 
-          <button onClick={handleClose}>I Agree</button>
-        </Box>
-      </Modal>
-    </div>
-  )
+               <button onClick={handleClose}>I Agree</button>
+            </motion.div>
+         </Modal>
+      </div>
+   )
 }
